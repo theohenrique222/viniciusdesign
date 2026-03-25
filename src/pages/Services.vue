789dbe21e1cdecfe2bedd8a368a1b4bc2c4cb2e7
theohenrique222 <theodoro222@hotmail.com>
2026-03-25 13:18:51 -0300

    refactor card component with grayscale background image, hover transition and overlay separation

--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -47,21 +47,28 @@ const bannersLoop = [...banners, ...banners, ...banners, ...banners, ...banners,
 
         <div class="container mx-auto grid lg:grid-cols-3 gap-10 px-10 py-20">
 
-            <div v-for="card in repeted" :key="card.id"
-                class="h-72 border bg-cover  border-neutral-800 hover:border-orange-400 transition flex flex-col justify-end cursor-pointer"
-                :class="card.imageUrl">
-                <div class="bg-neutral-950 w-full text-center p-2">
-                    <h3 class="text-2xl font-bold mb-4">
-                        {{ card.title }}
-                    </h3>
-                    <p class="text-gray-400">
-                        {{ card.text }}
-                    </p>
-                </div>
+    <div v-for="card in repeted" :key="card.id"
+        class="h-72 border border-neutral-800 hover:border-orange-400 transition flex flex-col justify-end cursor-pointer relative overflow-hidden">
 
-            </div>
-
+        <div 
+            class="absolute inset-0 bg-cover bg-center grayscale hover:grayscale-0 hover:scale-110 transition duration-700"
+            :class="card.imageUrl">
         </div>
+
+        <div class="absolute inset-0 bg-black/40"></div>
+
+        <div class="bg-neutral-950 p-4 m-4 flex justify-between relative z-10">
+            <h3 class="text-2xl font-bold">
+                {{ card.title }}
+            </h3>
+
+            <div class="absolute -top-6 right-6 bg-amber-500 rounded-full shadow-lg">
+                <i class="pi pi-verified p-3 text-white" style="font-size: 2rem"></i>
+            </div>
+        </div>
+
+    </div>
+</div>
 
     </section>
     <section class="bg-black">
@@ -81,98 +88,96 @@ const bannersLoop = [...banners, ...banners, ...banners, ...banners, ...banners,
     </section>
 
     <section>
-        
-            <div class="bg-surface-0 dark:bg-surface-950 px-6 md:px-12 lg:px-20 py-20 text-center">
-                <div class="flex flex-col gap-6">
-                    <div class="flex flex-col items-center gap-4">
-                        <div class="font-bold text-3xl leading-tight">
-                            <span class="text-surface-900 dark:text-surface-0">One Product, </span>
-                            <span class="text-primary-500 dark:text-primary-400">Many Solutions</span>
-                        </div>
-                        <div class="text-surface-500 dark:text-surface-400 leading-tight">Ac turpis egestas maecenas
-                            pharetra convallis posuere morbi leo urna.</div>
+
+        <div class="bg-surface-0 dark:bg-surface-950 px-6 md:px-12 lg:px-20 py-20 text-center">
+            <div class="flex flex-col gap-6">
+                <div class="flex flex-col items-center gap-4">
+                    <div class="font-bold text-3xl leading-tight">
+                        <span class="text-surface-900 dark:text-surface-0">One Product, </span>
+                        <span class="text-primary-500 dark:text-primary-400">Many Solutions</span>
                     </div>
-                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                        <div class="w-full rounded-md p-4">
-                            <span
-                                class="mb-4 flex h-16 w-16 items-center justify-center rounded-lg bg-surface-0 dark:bg-surface-800 shadow mx-auto">
-                                <i
-                                    class="pi pi-desktop text-2xl! lg:text-3xl! text-primary-500 dark:text-primary-400" />
-                            </span>
-                            <div class="flex flex-col gap-1">
-                                <div class="text-surface-900 dark:text-surface-0 text-xl font-medium leading-tight">
-                                    Built for Developers</div>
-                                <p class="text-surface-500 dark:text-surface-400 leading-normal">Duis aute irure dolor
-                                    in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                            </div>
+                    <div class="text-surface-500 dark:text-surface-400 leading-tight">Ac turpis egestas maecenas
+                        pharetra convallis posuere morbi leo urna.</div>
+                </div>
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    <div class="w-full rounded-md p-4">
+                        <span
+                            class="mb-4 flex h-16 w-16 items-center justify-center rounded-lg bg-surface-0 dark:bg-surface-800 shadow mx-auto">
+                            <i class="pi pi-desktop text-2xl! lg:text-3xl! text-primary-500 dark:text-primary-400" />
+                        </span>
+                        <div class="flex flex-col gap-1">
+                            <div class="text-surface-900 dark:text-surface-0 text-xl font-medium leading-tight">
+                                Built for Developers</div>
+                            <p class="text-surface-500 dark:text-surface-400 leading-normal">Duis aute irure dolor
+                                in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                         </div>
-                        <div class="w-full rounded-md p-4">
-                            <span
-                                class="mb-4 flex h-16 w-16 items-center justify-center rounded-lg bg-surface-0 dark:bg-surface-800 shadow mx-auto">
-                                <i class="pi pi-lock text-2xl! lg:text-3xl! text-primary-500 dark:text-primary-400" />
-                            </span>
-                            <div class="flex flex-col gap-1">
-                                <div class="text-surface-900 dark:text-surface-0 text-xl font-medium leading-tight">
-                                    End-to-End Encryption</div>
-                                <p class="text-surface-500 dark:text-surface-400 leading-normal">Risus nec feugiat in
-                                    fermentum posuere urna nec. Posuere sollicitudin aliquam ultrices sagittis.</p>
-                            </div>
+                    </div>
+                    <div class="w-full rounded-md p-4">
+                        <span
+                            class="mb-4 flex h-16 w-16 items-center justify-center rounded-lg bg-surface-0 dark:bg-surface-800 shadow mx-auto">
+                            <i class="pi pi-lock text-2xl! lg:text-3xl! text-primary-500 dark:text-primary-400" />
+                        </span>
+                        <div class="flex flex-col gap-1">
+                            <div class="text-surface-900 dark:text-surface-0 text-xl font-medium leading-tight">
+                                End-to-End Encryption</div>
+                            <p class="text-surface-500 dark:text-surface-400 leading-normal">Risus nec feugiat in
+                                fermentum posuere urna nec. Posuere sollicitudin aliquam ultrices sagittis.</p>
                         </div>
-                        <div class="w-full rounded-md p-4">
-                            <span
-                                class="mb-4 flex h-16 w-16 items-center justify-center rounded-lg bg-surface-0 dark:bg-surface-800 shadow mx-auto">
-                                <i
-                                    class="pi pi-face-smile text-2xl! lg:text-3xl! text-primary-500 dark:text-primary-400" />
-                            </span>
-                            <div class="flex flex-col gap-1">
-                                <div class="text-surface-900 dark:text-surface-0 text-xl font-medium leading-tight">Easy
-                                    to Use</div>
-                                <p class="text-surface-500 dark:text-surface-400 leading-normal">Ornare suspendisse sed
-                                    nisi lacus sed viverra tellus. Neque volutpat ac tincidunt vitae semper.</p>
-                            </div>
+                    </div>
+                    <div class="w-full rounded-md p-4">
+                        <span
+                            class="mb-4 flex h-16 w-16 items-center justify-center rounded-lg bg-surface-0 dark:bg-surface-800 shadow mx-auto">
+                            <i class="pi pi-face-smile text-2xl! lg:text-3xl! text-primary-500 dark:text-primary-400" />
+                        </span>
+                        <div class="flex flex-col gap-1">
+                            <div class="text-surface-900 dark:text-surface-0 text-xl font-medium leading-tight">Easy
+                                to Use</div>
+                            <p class="text-surface-500 dark:text-surface-400 leading-normal">Ornare suspendisse sed
+                                nisi lacus sed viverra tellus. Neque volutpat ac tincidunt vitae semper.</p>
                         </div>
-                        <div class="w-full rounded-md p-4">
-                            <span
-                                class="mb-4 flex h-16 w-16 items-center justify-center rounded-lg bg-surface-0 dark:bg-surface-800 shadow mx-auto">
-                                <i class="pi pi-globe text-2xl! lg:text-3xl! text-primary-500 dark:text-primary-400" />
-                            </span>
-                            <div class="flex flex-col gap-1">
-                                <div class="text-surface-900 dark:text-surface-0 text-xl font-medium leading-tight">Fast
-                                    & Global Support</div>
-                                <p class="text-surface-500 dark:text-surface-400 leading-normal">Fermentum et
-                                    sollicitudin ac orci phasellus egestas tellus rutrum tellus.</p>
-                            </div>
+                    </div>
+                    <div class="w-full rounded-md p-4">
+                        <span
+                            class="mb-4 flex h-16 w-16 items-center justify-center rounded-lg bg-surface-0 dark:bg-surface-800 shadow mx-auto">
+                            <i class="pi pi-globe text-2xl! lg:text-3xl! text-primary-500 dark:text-primary-400" />
+                        </span>
+                        <div class="flex flex-col gap-1">
+                            <div class="text-surface-900 dark:text-surface-0 text-xl font-medium leading-tight">Fast
+                                & Global Support</div>
+                            <p class="text-surface-500 dark:text-surface-400 leading-normal">Fermentum et
+                                sollicitudin ac orci phasellus egestas tellus rutrum tellus.</p>
                         </div>
-                        <div class="w-full rounded-md p-4">
-                            <span
-                                class="mb-4 flex h-16 w-16 items-center justify-center rounded-lg bg-surface-0 dark:bg-surface-800 shadow mx-auto">
-                                <i class="pi pi-github text-2xl! lg:text-3xl! text-primary-500 dark:text-primary-400" />
-                            </span>
-                            <div class="flex flex-col gap-1">
-                                <div class="text-surface-900 dark:text-surface-0 text-xl font-medium leading-tight">Open
-                                    Source</div>
-                                <p class="text-surface-500 dark:text-surface-400 leading-normal">Nec tincidunt praesent
-                                    semper feugiat. Sed adipiscing diam donec adipiscing tristique risus nec feugiat.
-                                </p>
-                            </div>
+                    </div>
+                    <div class="w-full rounded-md p-4">
+                        <span
+                            class="mb-4 flex h-16 w-16 items-center justify-center rounded-lg bg-surface-0 dark:bg-surface-800 shadow mx-auto">
+                            <i class="pi pi-github text-2xl! lg:text-3xl! text-primary-500 dark:text-primary-400" />
+                        </span>
+                        <div class="flex flex-col gap-1">
+                            <div class="text-surface-900 dark:text-surface-0 text-xl font-medium leading-tight">Open
+                                Source</div>
+                            <p class="text-surface-500 dark:text-surface-400 leading-normal">Nec tincidunt praesent
+                                semper feugiat. Sed adipiscing diam donec adipiscing tristique risus nec feugiat.
+                            </p>
                         </div>
-                        <div class="w-full rounded-md p-4">
-                            <span
-                                class="mb-4 flex h-16 w-16 items-center justify-center rounded-lg bg-surface-0 dark:bg-surface-800 shadow mx-auto">
-                                <i class="pi pi-shield text-2xl! lg:text-3xl! text-primary-500 dark:text-primary-400" />
-                            </span>
-                            <div class="flex flex-col gap-1">
-                                <div class="text-surface-900 dark:text-surface-0 text-xl font-medium leading-tight">
-                                    Trusted Security</div>
-                                <p class="text-surface-500 dark:text-surface-400 leading-normal">Mattis rhoncus urna
-                                    neque viverra justo nec ultrices. Id cursus metus aliquam eleifend.</p>
-                            </div>
+                    </div>
+                    <div class="w-full rounded-md p-4">
+                        <span
+                            class="mb-4 flex h-16 w-16 items-center justify-center rounded-lg bg-surface-0 dark:bg-surface-800 shadow mx-auto">
+                            <i class="pi pi-shield text-2xl! lg:text-3xl! text-primary-500 dark:text-primary-400" />
+                        </span>
+                        <div class="flex flex-col gap-1">
+                            <div class="text-surface-900 dark:text-surface-0 text-xl font-medium leading-tight">
+                                Trusted Security</div>
+                            <p class="text-surface-500 dark:text-surface-400 leading-normal">Mattis rhoncus urna
+                                neque viverra justo nec ultrices. Id cursus metus aliquam eleifend.</p>
                         </div>
                     </div>
                 </div>
             </div>
-        
-        
+        </div>
+
+
 
     </section>
 </template>
